71cd5b3b2d1f2082d1ec4f20522db220aee15e4f
Diego Capassi Moreira <diego.capassi.moreira@gmail.com>
2024-12-27 00:12:25 -0300

    Add files via upload

--- antenna/omni.js
+++ antenna/omni.js
@@ -2,5 +2,17 @@ const OMNI_GAIN = [
     {
         "angle": 0,
         "gain": 2.2
+    },
+    {
+        "angle": 90,
+        "gain": 2.2
+    },
+    {
+        "angle": 180,
+        "gain": 2.2
+    },
+    {
+        "angle": 270,
+        "gain": 2.2
     }
 ]
